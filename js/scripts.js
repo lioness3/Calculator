2bca3b9600c0fd0f7e7e3ee839628febbf36bed6
Student <example@example.com>
2019-10-21 14:21:18 -0700

    add Farhenheit to Celsius conversion

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,10 @@ var BMI = function (weight,height) {return (weight/(height*height));
 
 };
 
-var conversion = function(Farenheit,celcius)
-var weight = parseFloat(prompt("Enter your weight in lbs."));
-var height = parseInt(prompt("Enter your height in inches."));
+var conversion = function(Farenheit) {return ((Farenheit-32)*(5/9)); };
+//var weight = parseFloat(prompt("Enter your weight in lbs."));
+//var height = parseInt(prompt("Enter your height in inches."));
+var Farenheit = parseFloat(prompt("Enter the temperature in Farenheit"));
 // var number1 = parseInt(prompt("Enter a number:"));
 // var number2 = parseInt(prompt("Enter another number:"));
 //var result = add(number1,number2);
@@ -24,9 +25,11 @@ var height = parseInt(prompt("Enter your height in inches."));
 //var result = multiply(number1,number2);
 // var result = divide(number1,number2);
 
-var result = BMI(weight,height)*703;
+//var result = BMI(weight,height)*703;
+
+var celcius = conversion(Farenheit);
+
+alert(celcius);
 
 
-
-
-alert(result);
+//alert(result);
